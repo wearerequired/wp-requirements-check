e5d4990a0ab250e5ecc54c8046459a55fa61101c
Pascal Birchler <hello@pascalbirchler.ch>
2017-05-02 14:16:08 +0200

    Docs improvements

--- WP_Requirements_Check.php
+++ WP_Requirements_Check.php
@@ -12,12 +12,18 @@ class WP_Requirements_Check {
 	/**
 	 * Default name of the plugin.
 	 *
+	 * @since 1.0.0
+	 * @access private
+	 *
 	 * @var string
 	 */
 	private $title = '';
 
 	/**
 	 * Default minimum required PHP version.
+	 *
+	 * @since 1.0.0
+	 * @access private
 	 *
 	 * @var string
 	 */
@@ -26,6 +32,9 @@ class WP_Requirements_Check {
 	/**
 	 * Default minimum required WordPress version.
 	 *
+	 * @since 1.0.0
+	 * @access private
+	 *
 	 * @var string
 	 */
 	private $wp = '3.8';
@@ -33,12 +42,18 @@ class WP_Requirements_Check {
 	/**
 	 * Path to the main plugin file.
 	 *
+	 * @since 1.0.0
+	 * @access private
+	 *
 	 * @var string
 	 */
 	private $file;
 
 	/**
 	 * Constructor.
+	 *
+	 * @since 1.0.0
+	 * @access public
 	 *
 	 * @param array $args An array of arguments to overwrite the default requirements.
 	 */
@@ -53,10 +68,14 @@ class WP_Requirements_Check {
 	/**
 	 * Check if the install passes the requirements.
 	 *
+	 * @since 1.0.0
+	 * @access public
+	 *
 	 * @return bool True if the install passes the requirements, false otherwise.
 	 */
 	public function passes() {
 		$passes = $this->php_passes() && $this->wp_passes();
+
 		if ( ! $passes ) {
 			add_action( 'admin_notices', array( $this, 'deactivate' ) );
 		}
@@ -65,42 +84,53 @@ class WP_Requirements_Check {
 	}
 
 	/**
-	 * Deactivate the plugin again.
+	 * Deactivates the plugin again.
+	 *
+	 * @since 1.0.0
+	 * @access public
 	 */
 	public function deactivate() {
-		if ( isset( $this->file ) ) {
+		if ( null !== $this->file ) {
 			deactivate_plugins( plugin_basename( $this->file ) );
 		}
 	}
 
 	/**
-	 * Check if the PHP version passes the requirement.
+	 * Checks if the PHP version passes the requirement.
+	 *
+	 * @since 1.0.0
+	 * @access protected
 	 *
 	 * @return bool True if the PHP version is high enough, false otherwise.
 	 */
 	protected function php_passes() {
-		if ( $this->_php_at_least( $this->php ) ) {
+		if ( self::_php_at_least( $this->php ) ) {
 			return true;
-		} else {
-			add_action( 'admin_notices', array( $this, 'php_version_notice' ) );
-
-			return false;
 		}
+
+		add_action( 'admin_notices', array( $this, 'php_version_notice' ) );
+
+		return false;
 	}
 
 	/**
-	 * Compare the current PHP version with the minimum required version.
+	 * Compares the current PHP version with the minimum required version.
+	 *
+	 * @since 1.0.0
+	 * @access protected
 	 *
 	 * @param string $min_version The minimum required version.
-	 *
-	 * @return mixed
+	 * @return bool True if the PHP version is high enough, false otherwise.
 	 */
 	protected static function _php_at_least( $min_version ) {
-		return version_compare( phpversion(), $min_version, '>=' );
+		return version_compare( PHP_VERSION, $min_version, '>=' );
 	}
 
 	/**
-	 * Show the PHP version notice.
+	 * Displays the PHP version notice.
+	 *
+	 * @since 1.0.0
+	 * @access public
 	 */
 	public function php_version_notice() {
 		?>
@@ -113,31 +143,39 @@ class WP_Requirements_Check {
 	/**
 	 * Check if the WordPress version passes the requirement.
 	 *
+	 * @since 1.0.0
+	 * @access protected
+	 *
 	 * @return bool True if the WordPress version is high enough, false otherwise.
 	 */
 	protected function wp_passes() {
-		if ( $this->_wp_at_least( $this->wp ) ) {
+		if ( self::_wp_at_least( $this->wp ) ) {
 			return true;
-		} else {
-			add_action( 'admin_notices', array( $this, 'wp_version_notice' ) );
-
-			return false;
 		}
+
+		add_action( 'admin_notices', array( $this, 'wp_version_notice' ) );
+
+		return false;
 	}
 
 	/**
 	 * Compare the current WordPress version with the minimum required version.
 	 *
-	 * @param string $min_version Minimum required WordPress version.
+	 * @since 1.0.0
+	 * @access protected
 	 *
-	 * @return mixed
+	 * @param string $min_version Minimum required WordPress version.
+	 * @return bool True if the WordPress version is high enough, false otherwise.
 	 */
-	protected function _wp_at_least( $min_version ) {
+	protected static function _wp_at_least( $min_version ) {
 		return version_compare( get_bloginfo( 'version' ), $min_version, '>=' );
 	}
 
 	/**
-	 * SHow the WordPress version notice.
+	 * Show the WordPress version notice.
+	 *
+	 * @since 1.0.0
+	 * @access public
 	 */
 	public function wp_version_notice() {
 		?>
